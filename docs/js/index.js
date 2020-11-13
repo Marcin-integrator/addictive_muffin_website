b95d90158a749c07a1be479adc728f85b265af12
Martinjr8 <marcinmarcin8jr@gmail.com>
2020-11-13 19:42:02 +0100

    test_1 for preview

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,6 +1,6 @@
 import '../css/album.css';
 import '../css/reset.css';
-import '../css/bootstrap.min.css';
+import '../css/bootstrap.css';
 import '../css/styles.css';
 
 import '../img/watercolor-bokeh-pastel-background.jpg';
@@ -8,13 +8,6 @@ import '../img/watercolor-bokeh-pastel-background.jpg';
 import './bootstrap.bundle.min.js';
 
 import './filter.js';
-/*
-function importAll(r) {
-    return r.keys().map(r);
-  }
-  
-  const images = importAll(require.context('../img', false, /\.(png|jpe?g|svg)$/));*/
 
 var requireContext = require.context("../img", true, /^\.\/.*\.jpg$/);
 requireContext.keys().map(requireContext);
-console.log(requireContext);
